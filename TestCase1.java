74b1b4fc07ef3d993128f30eef87ed6b96183a1c
popescutiberiu <100191464+popescutiberiu@users.noreply.github.com>
2024-12-15 17:54:25 +0200

    Update TestCase1.java

--- TestCase1.java
+++ TestCase1.java
@@ -1,1 +1,14 @@
-#test_case1
+public class TestCase1 {
+    public static void main(String[] args) {
+        int result = add(3, 2);
+        if (result == 5) {
+            System.out.println("TestCase1 Passed: 3 + 2 = " + result);
+        } else {
+            System.out.println("TestCase1 Failed: 3 + 2 != " + result);
+        }
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}

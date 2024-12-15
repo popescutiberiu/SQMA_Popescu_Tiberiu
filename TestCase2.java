b2251c6738808c79128ca8babc511e47f0d2c720
popescutiberiu <100191464+popescutiberiu@users.noreply.github.com>
2024-12-15 17:55:00 +0200

    Update TestCase2.java

--- TestCase2.java
+++ TestCase2.java
@@ -1,1 +1,14 @@
-# test_case2
+public class TestCase2 {
+    public static void main(String[] args) {
+        int result = multiply(2, 3);
+        if (result == 6) {
+            System.out.println("TestCase2 Passed: 2 * 3 = " + result);
+        } else {
+            System.out.println("TestCase2 Failed: 2 * 3 != " + result);
+        }
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
